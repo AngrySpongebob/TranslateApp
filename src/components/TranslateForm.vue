4b9621590447559eef79f89319166519692a9ee2
2454024168@qq.com <2454024168@qq.com>
2018-09-20 15:55:25 +0800

    分支

---
+++ src/components/TranslateForm.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="row" id="translateForm">
+	  <div class="col-md-offset-3 col-md-6">
+	
+		  <form class="jumbotron form-inline" v-on:submit='formSubmit'>
+			<input class="form-control" type='text' v-model='textToTranslate' placeholder='Input what you want to translate,please.'>
+			<select class="form-control osele" v-model='lang'>
+				<option value='en'>英文</option>
+				<option value='zh'>中文</option>
+
+			</select>
+			<input class="btn btn-primary obtn" type='submit' value='Translate'>
+		</form>
+	</div>
+  </div>
+</template>
+
+<script>
+
+	export default {
+	  name: 'translateForm',
+	  data:function(){
+		return {
+			textToTranslate:'',
+			lang:'en'
+		}
+	  },
+	  methods:{
+		formSubmit:function(e){
+			this.$emit('formSub', this.textToTranslate,this.lang); //$emit(父组件中接受的值,data) 子组件向父组件传值
+			e.preventDefault(); // 禁用默认事件
+		}
+	  }
+	}
+</script>
+
+<style>
+	.osele{
+		margin-left: 10px;
+	}
+	.obtn{
+		margin-left: 10px;
+	}
+</style>
---
+++ src/components/TranslateOutput.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="translateOutput">
+	  <ul class="list-unstyled">
+		  <li v-for='text in arrayTranslate'><h4>{{text}}</h4></li>
+	</ul>
+<!--    <h5>{{arrayTranslate}}</h5>-->
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'arrayTranslate',
+  props:['arrayTranslate']
+}
+</script>
+
+<style>
+
+</style>
